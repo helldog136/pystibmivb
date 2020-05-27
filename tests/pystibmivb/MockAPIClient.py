5b33d4ba5f224a17aa42f91326422cc740bf7fcf
Helldog136 <130792@student.umons.ac.be>
2020-05-27 18:14:42 +0200

    added missing sample gtfs zip for unittests

--- tests/pystibmivb/MockAPIClient.py
+++ tests/pystibmivb/MockAPIClient.py
@@ -3,7 +3,7 @@ import datetime
 
 from pystibmivb import AbstractSTIBAPIClient
 from pystibmivb.service.STIBService import PASSING_TIME_BY_POINT_SUFFIX
-from pystibmivb.service.ShapefileService import ENDPOINT_SHAPEFILES
+from pystibmivb.service.ShapefileService import ENDPOINT_SHAPEFILES, ENDPOINT_GTFS
 
 
 class MockAPIClient(AbstractSTIBAPIClient):
@@ -12,10 +12,14 @@ class MockAPIClient(AbstractSTIBAPIClient):
     def __init__(self):
         with open("../resources/shapefiles.zip", 'rb') as sf:
             self.shapefilezipcontent = sf.read()
+        with open("../resources/gtfs.zip", 'rb') as sf:
+            self.gtfszipcontent = sf.read()
 
     async def api_call(self, endpoint_suffix: str, additional_headers=None):
         if endpoint_suffix == ENDPOINT_SHAPEFILES:
             return self.shapefilezipcontent
+        elif endpoint_suffix == ENDPOINT_GTFS:
+            return self.gtfszipcontent
         elif endpoint_suffix.startswith(PASSING_TIME_BY_POINT_SUFFIX):
             if endpoint_suffix.endswith("3755"):
                 now = datetime.datetime.now()
